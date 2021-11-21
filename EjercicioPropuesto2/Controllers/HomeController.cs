ed7baba4a03e4f52b3bd7f2f3139b75967b8f9bf
DiegoHuamanR <72468973@ucsm.edu.pe>
2021-11-21 18:33:45 -0500

    Ejercicio Propuesto 2

---
+++ EjercicioPropuesto2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EjercicioPropuesto2.Controllers
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SetCookie(string valor)
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Description of Enterprise DDA.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contactanos en:";
+
+            return View();
+        }
+    }
+}
