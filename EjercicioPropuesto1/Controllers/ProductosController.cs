1c215a23d5cd14f654218c3d57583ee8f61b98cf
DiegoHuamanR <72468973@ucsm.edu.pe>
2021-11-24 08:44:17 -0500

    Ejercicio Propuesto 1

---
+++ EjercicioPropuesto1/Controllers/ProductosController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EjercicioPropuesto1.Controllers
+{
+    public class ProductosController : Controller
+    {
+        [Authorize]
+        // GET: Productos
+        public ActionResult mostrarproductos()
+        {
+            return View();
+        }
+    }
+}
---
+++ EjercicioPropuesto1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EjercicioPropuesto1.Startup))]
+namespace EjercicioPropuesto1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
